286f895201b4cb2a0c354277056a74d33697a55a
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-30 05:17:48 +0900

    Attribute binding

--- 03-vue.js-basics(part2)/app.js
+++ 03-vue.js-basics(part2)/app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'http://www.sacode.web.id',
             showBooks: true,
             books: [
-                { title: 'name of the wind', author: 'patrick rothfuss' },
-                { title: 'the way of kings', author: 'brandong sanderson' },
-                { title: 'the final empire', author: 'brandong sanderson' },
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg' },
+                { title: 'the way of kings', author: 'brandong sanderson', img: 'assets/2.jpg' },
+                { title: 'the final empire', author: 'brandong sanderson', img: 'assets/3.jpg' },
             ]
         }
     },
