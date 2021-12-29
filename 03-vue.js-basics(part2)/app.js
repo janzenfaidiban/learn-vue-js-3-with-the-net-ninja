ac1273b9ae3e5836c8dd494ed531eb2ad351c8fc
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-30 05:41:55 +0900

    Challenge: toggling favs

--- 03-vue.js-basics(part2)/app.js
+++ 03-vue.js-basics(part2)/app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
         return {
             url: 'http://www.sacode.web.id',
             showBooks: true,
+            changeColor: true,
             books: [
                 { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
                 { title: 'the way of kings', author: 'brandong sanderson', img: 'assets/2.jpg', isFav: false },
@@ -15,18 +16,15 @@ const app = Vue.createApp({
         toggleShowBooks() {
             this.showBooks = !this.showBooks
         },
-        handleEvent(e, data) {
-            console.log(e, e.type)
-            if(data) {
-                console.log(data)
-            }
-        },
-        handleMousemove(e) {
-            this.x = e.offsetX
-            this.y = e.offsetY
+        toggleFav(book) {
+            book.isFav = !book.isFav
         }
     }
 
 })
 
 app.mount('#app')
+
+// Challenge - Add to Favs
+// - attach a click event to each li tag (for each book)
+// - when user clicks an li, toggle the 'isFav' property of that book
