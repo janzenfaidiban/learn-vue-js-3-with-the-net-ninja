48e90d8950b2d2a5c0d019f1a2ce92775836ac83
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-30 05:24:44 +0900

    Dynamic classes

--- 03-vue.js-basics(part2)/app.js
+++ 03-vue.js-basics(part2)/app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             url: 'http://www.sacode.web.id',
             showBooks: true,
             books: [
-                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg' },
-                { title: 'the way of kings', author: 'brandong sanderson', img: 'assets/2.jpg' },
-                { title: 'the final empire', author: 'brandong sanderson', img: 'assets/3.jpg' },
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+                { title: 'the way of kings', author: 'brandong sanderson', img: 'assets/2.jpg', isFav: false },
+                { title: 'the final empire', author: 'brandong sanderson', img: 'assets/3.jpg', isFav: true },
             ]
         }
     },
