05c22ae33561619dddaa517a18580451141c7156
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-30 05:08:23 +0900

    Outputting lists (v-for)

--- 03-vue.js-basics(part2)/app.js
+++ 03-vue.js-basics(part2)/app.js
@@ -2,11 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The final empire now',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss' },
+                { title: 'the way of kings', author: 'brandong sanderson' },
+                { title: 'the final empire', author: 'brandong sanderson' },
+            ]
         }
     },
 
