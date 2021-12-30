2379c71b415298474ae8b2de296d738ce0ecfac3
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-30 18:21:08 +0900

    Props

--- 04/modal-project/src/App.vue
+++ 04/modal-project/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal />
+  <Modal :header="header" :text="text" theme="sale" />
 </template>
 
 <script>
@@ -13,7 +13,9 @@ export default {
 
   data() {
     return {
-      title: 'My First Vue App :)'
+      title: 'My First Vue App :)',
+      header: 'Sign up for the Giveaway',
+      text: 'Grab your ninja swag for half price!'
     }
   }
 
