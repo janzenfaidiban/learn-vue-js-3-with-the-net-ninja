176349591ce8f979e4a0d989b57c336d96bd1457
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-31 00:16:37 +0900

    Starting a new game

---
+++ 06/reaction-timer/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Ninja Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">play</button>
+    <Block v-if="isPlaying" :delay="delay" />
+  </div>
+</template>
+
+<script>
+
+import Block from './components/Block.vue'
+
+export default {
+
+  name: 'App',
+
+  components: { 
+    Block 
+  },
+
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
