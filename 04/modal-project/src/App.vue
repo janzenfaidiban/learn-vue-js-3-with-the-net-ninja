ca8e6e0fc768e6051da93878ed7f9128d462a120
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-30 19:12:32 +0900

    Slots

--- 04/modal-project/src/App.vue
+++ 04/modal-project/src/App.vue
@@ -2,7 +2,14 @@
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
   <div v-if="showModal">
-    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+    <Modal theme="" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Ninja Givaway!!</h1>
+      <p>Grab your ninja swag for half price!!</p>
+    </Modal>
   </div>
   <button @click.alt="toggleModal">open modal (alt)</button>
 </template>
