11577ae339b0bb0d6546baf82531304aa372ffc4
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-31 02:33:47 +0900

    select field

--- 07/web-form/src/components/SignupForm.vue
+++ 07/web-form/src/components/SignupForm.vue
@@ -8,10 +8,17 @@
         <label>Password</label>
         <input type="password" required v-model="password">
 
+        <label>Role:</label>
+        <select v-model="role">
+            <option value="developer">Web Developer</option>
+            <option value="designer">Web Designer</option>
+        </select>
+
     </form>
 
     <p>Email : {{ email }}</p>
     <p>Password : {{ password }}</p>
+    <p>Role : {{ role }}</p>
 
 </template>
 
@@ -20,7 +27,8 @@ export default {
     data() {
         return {
             email: '',
-            password: ''
+            password: '',
+            role: ''
         }
     }
 }
@@ -43,7 +51,7 @@ export default {
         letter-spacing: 1px;
         font-weight: bold;
     }
-    input {
+    input, select {
         display: block;
         padding: 10px 6px;
         width: 100%;
